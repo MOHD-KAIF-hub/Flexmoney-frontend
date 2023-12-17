ad59b0257acc68fa5a0128932fd31c12cb80d51b
MOHD-KAIF-hub <mohdkaif30062002@gmail.com>
2023-12-17 22:27:34 +0530

    last commit

--- report/src/App.js
+++ report/src/App.js
@@ -1,13 +1,11 @@
 
 import React from 'react'
-import SubmitCaseForm from './components/SubmitCaseForm'
 import './App.css'
 import YogaAdmissionForm from './components/YogaAdmissionForm'
 
 const App = () => {
   return (
     <div className='form'>
-      {/* <SubmitCaseForm/> */}
       <YogaAdmissionForm/>
     </div>
   )
