6917ae5cd402aa683970ca8ba32ff4d625cc31d0
MOHD-KAIF-hub <mohdkaif30062002@gmail.com>
2023-12-17 21:53:48 +0530

    my frontendcommit

---
+++ report/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import SubmitCaseForm from './components/SubmitCaseForm'
+import './App.css'
+import YogaAdmissionForm from './components/YogaAdmissionForm'
+
+const App = () => {
+  return (
+    <div className='form'>
+      {/* <SubmitCaseForm/> */}
+      <YogaAdmissionForm/>
+    </div>
+  )
+}
+
+export default App
